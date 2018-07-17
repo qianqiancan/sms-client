f98fb2202205e73236eea39096aea07d37582e54
qianqianyan <65542675@qq.com>
2018-07-17 12:31:27 +0800

    first commit

---
+++ src/lib/slideMenuItems.js
@@ -0,0 +1,66 @@
+module.exports = [
+  {
+    type: 'item',
+    icon: 'fa fa-circle-o',
+    name: '首页',
+    router: {
+      name: 'Sample'
+    }
+  },
+  {
+    type: 'item',
+    icon: 'fa fa-circle-o',
+    name: '用户管理',
+    router: {
+      name: 'DashboardV1'
+    }
+  },
+  {
+    type: 'item',
+    icon: 'fa fa-circle-o',
+    name: '分组管理',
+    router: {
+      name: 'DashboardV2'
+    }
+  },
+  {
+    type: 'item',
+    icon: 'fa fa-circle-o',
+    name: '发送短信',
+    router: {
+      name: 'InfoBoxExample'
+    }
+  },
+  {
+    type: 'item',
+    icon: 'fa fa-circle-o',
+    name: '短信记录',
+    router: {
+      name: 'ChartExample'
+    }
+  },
+  {
+    type: 'item',
+    icon: 'fa fa-circle-o',
+    name: 'Alert',
+    router: {
+      name: 'AlertExample'
+    }
+  },
+  {
+    type: 'item',
+    icon: 'fa fa-circle-o',
+    name: 'Modal',
+    router: {
+      name: 'ModalExample'
+    }
+  },
+  {
+    type: 'item',
+    icon: 'fa fa-circle-o',
+    name: 'APIExample',
+    router: {
+      name: 'APIExample'
+    }
+  }
+]
---
+++ src/vuex/modules/messages.js
@@ -0,0 +1,37 @@
+const state = {
+  main: [
+    {
+      id: 1,
+      src: 'static/img/user2-160x160.jpg',
+      header: 'Support Team',
+      content: 'Why not buy a new awesome theme?',
+      tm: ' 5 mins',
+      body: 'Message 1'
+    },
+    {
+      id: 2,
+      src: 'static/img/user2-160x160.jpg',
+      header: 'Design Team',
+      content: 'hhhjjjjj?',
+      tm: ' 2 hours',
+      body: 'Message 2'
+    },
+    {
+      id: 3,
+      src: 'static/img/user4-128x128.jpg',
+      header: 'Developers',
+      content: 'Why not buy a new awesome theme?',
+      tm: ' Today',
+      body: 'Message 3'
+    }
+  ]
+}
+
+const mutations = {
+
+}
+
+export default {
+  state,
+  mutations
+}
