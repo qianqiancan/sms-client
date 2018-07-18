1c25c4f24d656b8c9f642d4d4e32166dfa592e84
qianqianyan <65542675@qq.com>
2018-07-18 21:13:55 +0800

    更改几个界面

---
+++ src/examples/UserEdit.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="dashboard">
+    <row>
+      <el-form :model="form">
+        <el-form-item label="姓名" :label-width="formLabelWidth">
+          <el-input v-model="form.name" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" :label-width="formLabelWidth">
+          <el-input v-model="form.phone" >
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="alert(123)" type="primary">确定</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
+    </row>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        form: {
+          name: '',
+          phone: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        formLabelWidth: '120px'
+      }
+    },
+    methods: {
+      tableRowClassName: function ({row, rowIndex}) {
+        if (rowIndex % 2 === 1) {
+          return 'warning-row'
+        }
+        return 'success-row'
+      }
+    },
+    components: {
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/examples/UserManager.vue
+++ src/examples/UserManager.vue
@@ -13,7 +13,8 @@
       <div class="col-md-12 col-sm-12 col-xs-12">
         <el-table
           :data="tableData"
-          style="width: 100%">
+          style="width: 100%"
+          :row-class-name="tableRowClassName">
           <el-table-column
             prop="date"
             label="日期"
@@ -25,74 +26,51 @@
             width="180">
           </el-table-column>
           <el-table-column
-            prop="address"
-            label="地址">
+            prop="phone"
+            label="电话">
           </el-table-column>
         </el-table>
       </div>
     </row>
     <el-dialog title="新增用户" :visible.sync="dialogFormVisible">
-      <el-form :model="form">
-        <el-form-item label="姓名" :label-width="formLabelWidth">
-          <el-input v-model="form.name" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="电话" :label-width="formLabelWidth">
-          <el-input v-model="form.phone" >
-          </el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary">确定</el-button>
-          <el-button>取消</el-button>
-        </el-form-item>
-      </el-form>
+      <user-edit></user-edit>
     </el-dialog>
   </section>
 
 </template>
 
 <script>
-
+    import UserEdit from './UserEdit.vue'
     export default {
       data: function () {
         return {
+          dialogFormVisible: false,
           tableData: [{
             date: '2016-05-02',
             name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
+            phone: '13333333333'
           }, {
             date: '2016-05-04',
             name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄'
+            phone: '13222222222'
           }, {
             date: '2016-05-01',
             name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
+            phone: '13111111111'
           }, {
             date: '2016-05-03',
             name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          }],
-          dialogFormVisible: false,
-          form: {
-            name: '',
-            phone: '',
-            date1: '',
-            date2: '',
-            delivery: false,
-            type: [],
-            resource: '',
-            desc: ''
-          },
-          formLabelWidth: '120px'
+            phone: '13444444444'
+          }]
         }
       },
       methods: {
       },
       components: {
+        'user-edit': UserEdit
       }
     }
 </script>
 
 <style scoped>
-
 </style>
