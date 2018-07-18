01b58e5437a87779c3a09d7c17b278f8ae736b7d
niedongye <1778536508@qq.com>
2018-07-19 02:06:14 +0800

    增加 全部删除

--- src/examples/UserEdit.vue
+++ src/examples/UserEdit.vue
@@ -6,12 +6,12 @@
           <el-input v-model="form.name" auto-complete="off"></el-input>
         </el-form-item>
         <el-form-item label="电话" :label-width="formLabelWidth">
-          <el-input v-model="form.phone" >
+          <el-input v-model="form.phone">
           </el-input>
         </el-form-item>
         <el-form-item>
-          <el-button @click="alert(123)" type="primary">确定</el-button>
-          <el-button>取消</el-button>
+          <el-button @click="btnCacel(1)" type="primary">确定</el-button>
+          <el-button @click="btnCacel(2)">取消</el-button>
         </el-form-item>
       </el-form>
     </row>
@@ -41,6 +41,16 @@
           return 'warning-row'
         }
         return 'success-row'
+      },
+      btnCacel (mun) {
+        var date = new Date()
+        console.log('0' + (date.getMonth() + 1))
+        var month = date.getMonth() + 1 > 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1)
+        var rdata = date.getDate() > 9 ? date.getDate() : '0' + date.getDate()
+        this.form.date1 = date.getFullYear() + '-' + month + '-' + rdata
+        this.$emit('btn-cacel', { msg: false, data: mun === 1 ? this.form : '' })
+        this.form.name = ''
+        this.form.phone = ''
       }
     },
     components: {
--- src/examples/UserManager.vue
+++ src/examples/UserManager.vue
@@ -5,14 +5,15 @@
       <div class="col-md-12 col-sm-12 col-xs-12">
         <el-button @click="dialogFormVisible = true" plain>新增</el-button>
         <el-button type="primary" plain>导入</el-button>
-        <el-button type="primary" plain>编辑</el-button>
-        <el-button type="success" plain>删除</el-button>
-        <el-button type="danger" plain>全部删除</el-button>
+        <el-button type="primary" @click="dialogFormEdit = true" plain>编辑</el-button>
+        <el-button type="success" @click="removeData" plain>删除</el-button>
+        <el-button type="danger" @click="tableData = []" plain>全部删除</el-button>
       </div>
     </row>
     <row>
       <div class="col-md-12 col-sm-12 col-xs-12">
         <el-table
+          @selection-change="changeFun"
           :data="tableData"
           style="width: 100%"
           :row-class-name="tableRowClassName">
@@ -38,7 +39,10 @@
       </div>
     </row>
     <el-dialog title="新增用户" :visible.sync="dialogFormVisible">
-      <user-edit></user-edit>
+      <user-edit @btn-cacel="btnCacel" @btn-deter="btnCacel"></user-edit>
+    </el-dialog>
+    <el-dialog title="编辑用户" :visible.sync="dialogFormEdit">
+      <user-edit @btn-cacel="btnCacel" @btn-deter="btnCacel"></user-edit>
     </el-dialog>
   </section>
 
@@ -50,26 +54,62 @@
       data: function () {
         return {
           dialogFormVisible: false,
+          dialogFormEdit: false,
           tableData: [{
+            id: 1,
             date: '2016-05-02',
             name: '王小虎',
             phone: '13333333333'
           }, {
+            id: 2,
             date: '2016-05-04',
             name: '王小虎',
             phone: '13222222222'
           }, {
+            id: 3,
             date: '2016-05-01',
             name: '王小虎',
             phone: '13111111111'
           }, {
+            id: 4,
             date: '2016-05-03',
             name: '王小虎',
             phone: '13444444444'
-          }]
+          }],
+          editVal: [],
+          indexData: []
         }
       },
       methods: {
+        btnCacel (obj) {
+          console.log(obj)
+          this.dialogFormVisible = obj.msg
+          if (obj.data !== '') {
+            this.tableData.push({
+              date: obj.data.date1,
+              name: obj.data.name,
+              phone: obj.data.phone
+            })
+          }
+        },
+        changeFun (data) {
+          console.log('val==>', data)
+          this.editVal.push(data)
+          let _this = this
+          data.forEach((val, index) => {
+            _this.tableData.forEach((v, i) => {
+              // id 是每一行的数据id
+              if (val.id === v.id) {
+                console.log('索引值', i)
+                _this.indexData.push(i)
+              }
+            })
+          })
+        },
+        removeData () {
+          console.log('选中的数据', this.editVal)
+          console.log('选中的索引值', this.indexData)
+        }
       },
       components: {
         'user-edit': UserEdit
